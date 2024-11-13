328b8322be545a1e66458f5cbe7b795e937aac70
Gigio-am <earistizabal61@misena.edu.co>
2024-11-13 10:38:01 -0500

    Se modifica el formulario por el que sirve

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -3,29 +3,36 @@ include 'db_connection.php';
 
 // Obtener datos del formulario
 $identificacion = $_POST['identificacion'];
-$nombre = $_POST['nombre'];
+$nombres = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
+$edad = (int)$_POST['edad'];
+$pais = $_POST['pais'];
+$ciudad = $_POST['ciudad'];
+$direccion = $_POST['direccion'];
+$telefono = $_POST['telefono'];
+$correo_electronico = $_POST['correo_electronico'];
+$mensaje = $_POST['mensaje'];
+$autorizacion = isset($_POST['autorizacion']) ? 1 : 0;
 
+// Establece un usuario_id_usuario existente
+$usuario_id_usuario = 1; // Cambia a un valor que exista en la tabla usuario
 
+// Insertar los datos en la tabla beneficiario
+$sql = "INSERT INTO beneficiario (usuario_id_usuario, identificacion, nombres, apellidos, edad, pais, ciudad, direccion, telefono, correo_electronico, mensaje, autorizacion) 
+VALUES ($usuario_id_usuario, '$identificacion', '$nombres', '$apellidos', $edad, '$pais', '$ciudad', '$direccion', '$telefono', '$correo_electronico', '$mensaje', $autorizacion)";
 
-$insertar = "INSERT INTO beneficiario VALUES ('$identificacion', '$nombre', '$apellidos')";
-
-$query = mysqli_query($conectar, $insertar);
-
-if($query){
-
-   echo "<script> alert('correcto');
-    location.href = 'beneficiarios.html';
-   </script>";
-
-}else{
-    echo "<script language='javaScript'>
-//         alert('ERROR:los datos no fueron modificados correctamente al Registro');
-//         location.assign('beneficiarios.html')
-//         </script>";
+if ($conexion->query($sql) === TRUE) {
+    echo "Datos guardados exitosamente";
+    // Esperar 2 segundos antes de redirigir
+    header("refresh:2; url=menu_beneficiario.php");
+    // O puedes redirigir inmediatamente sin mensaje con:
+    // header("Location: menu_beneficiario.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conexion->error;
 }
 
-
-
+// Cerrar la conexión
+$conexion->close();
 ?>
+
 
